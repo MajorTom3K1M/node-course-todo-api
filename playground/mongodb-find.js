dbe9d88aeaf60033e2287ebc88fbea7e7a8cc161
MajorTom3K1M <invaderzgame@gmail.com>
2018-07-10 14:46:04 +0700

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server.');
+  }
+  console.log('Connected to MongoDB server.');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5b41b8b833d06d0921aa330a')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to feth todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to feth todos', err);
+  // });
+
+  db.collection('Users').find({
+    name: 'Tom'
+  }).toArray().then((result) => {
+    console.log(JSON.stringify(result, undefined, 2));
+  }, (err) => {
+    console.log('Unable to feth users ',err);
+  });
+
+});
