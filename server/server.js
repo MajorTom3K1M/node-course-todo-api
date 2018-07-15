8fa2a4a499632a95210448016ee6f42f58ca8b33
MajorTom3K1M <invaderzgame@gmail.com>
2018-07-15 22:27:47 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -26,6 +28,32 @@ app.get('/todos', (req, res) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/123456
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //Valid id using isValid
+  if(!ObjectID.isValid(id)) {
+    //404 - send back empty send
+    return res.status(404).send();
+  }
+
+  //findById
+  Todo.findById(id).then((todo) => {
+    //success
+      if(!todo){
+        //if no todo - send back 404 with empty body
+        return res.status(404).send();
+      }
+      //if todo - send it back
+      res.send({todo});
+  },(e) => {
+    //error
+      //400 - and send empty body back
+    res.status(400).send();
   });
 });
 
